471760bc4e39be9d186435eaed5579daa2b595b3
James Ahlm <14782828428@qq.com>
2016-11-09 22:45:41 +0800

    Installed Request Module

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express')
 var app = express()
+var key = "1cb53ae3e0d04d9c99bfe629b8b6d3be";
+var query = "sushi";
+var queryaddr = "http://api.bing.net/json.aspx?Appid=" + key + "&query=" + query + "&sources=web"
 
 app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/search/:query', function (req, res) {
+  var jsonResp = 'l'//Query Bing, Use Request to make a call
   res.send('Hello World!')
 })
 
