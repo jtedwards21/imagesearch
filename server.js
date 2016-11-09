7114284e4f2366cad7905d1c5a8a3a2669338e8a
James Ahlm <14782828428@qq.com>
2016-11-09 23:04:44 +0800

    Added database connection

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express')
 var app = express()
 var request = require('request')
-//Install and Configure db
+var databaseUrl = "mongodb://localhost:27017/isearchdb";
+var mongojs = require("mongojs");
+var db = mongojs("isearchdb", ["isearches"]);
 var key = "1cb53ae3e0d04d9c99bfe629b8b6d3be";
 var query = "sushi";
 var queryaddr = "http://api.bing.net/json.aspx?Appid=" + key + "&query=" + query + "&sources=web"
 
 app.get('/', function (req, res) {
+  //Show the last ten searches
+  
   res.send('Hello World!')
 })
 
@@ -14,6 +18,12 @@ app.get('/search/:query', function (req, res) {
 　　request(queryaddr, function(error, response, body){
     if (!error && response.statusCode == 200){
 	console.log(body)//Get from JSON
+
+	//Place search data in database (term, when)
+	db.isearches.insert({term: req.params.query, when: Date.now()})
+
+	//Return JSON data
+	
     }
   
   });
