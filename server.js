26f3a8bfe93b2b652a85a524fb8bbf6b30a8ef02
James Ahlm <14782828428@qq.com>
2016-11-09 22:53:22 +0800

    Can Receive the JSON Data from the api

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
+var request = require('request')
+//Install and Configure db
 var key = "1cb53ae3e0d04d9c99bfe629b8b6d3be";
 var query = "sushi";
 var queryaddr = "http://api.bing.net/json.aspx?Appid=" + key + "&query=" + query + "&sources=web"
@@ -9,9 +11,13 @@ app.get('/', function (req, res) {
 })
 
 app.get('/search/:query', function (req, res) {
-  var jsonResp = 'l'//Query Bing, Use Request to make a call
-  res.send('Hello World!')
-})
+　　request(queryaddr, function(error, response, body){
+    if (!error && response.statusCode == 200){
+	console.log(body)//Get from JSON
+    }
+  
+  });
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
