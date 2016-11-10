1d5accafe35d3e1b4bee48ffc7f49333dd16640f
James Ahlm <14782828428@qq.com>
2016-11-10 09:01:59 +0800

    Can recieve image data from the bing server

--- server.js
+++ server.js
@@ -4,9 +4,29 @@ var request = require('request')
 var databaseUrl = "mongodb://localhost:27017/isearchdb";
 var mongojs = require("mongojs");
 var db = mongojs("isearchdb", ["isearches"]);
-var key = "1cb53ae3e0d04d9c99bfe629b8b6d3be";
+var key = "55cc5f31c4d943ebbf0d522481bab5cd";
 var query = "sushi";
-var queryaddr = "http://api.bing.net/json.aspx?Appid=" + key + "&query=" + query + "&sources=web"
+
+function m_options(q, count, offset){
+  var url = "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=" + q + "&count=1" + count + "&offset=" + offset + "&mkt=en-us&safeSearch=Moderate";
+  return {
+  	url: url,
+  	headers: {
+	"Ocp-Apim-Subscription-Key": key
+ 	}
+　　}
+}
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+	var info = JSON.parse(body);
+	console.log(info);
+	//Place search data in database (term, when)
+	//db.isearches.insert({term: req.params.query, when: Date.now()})
+
+	//Return JSON data
+  }
+}
 
 app.get('/', function (req, res) {
   //Show the last ten searches
@@ -15,18 +35,8 @@ app.get('/', function (req, res) {
 })
 
 app.get('/search/:query', function (req, res) {
-　　request(queryaddr, function(error, response, body){
-    if (!error && response.statusCode == 200){
-	console.log(body)//Get from JSON
-
-	//Place search data in database (term, when)
-	db.isearches.insert({term: req.params.query, when: Date.now()})
-
-	//Return JSON data
-	
-    }
-  
-  });
+  var options = m_options("cats", 10, 0);
+　　request(options, callback);
 });
 
 app.listen(3000, function () {
